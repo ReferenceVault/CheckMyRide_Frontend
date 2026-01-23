4269cbb3d846cc02fd9193fcb5b291661d20bb29
Ubuntu <ubuntu@ip-172-31-4-24.ca-central-1.compute.internal>
2026-01-23 05:24:51 +0000

    Build and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-git pull origin main
-npm install
-npm run build
-pm2 delete frontend
-pm2 start "npm start" --name frontend
+
+pm2 delete frontend || true
+pm2 start npm --name "frontend" -- start
+pm2 save
+
